42962e96ae6bde428802525b1e71110c40f60b12
moon <37049001+moonclau@users.noreply.github.com>
2018-10-14 13:53:05 -0500

    registro1.1

---
+++ src/main/java/com/sap/inventario/servlets/AgregarMerma.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sap.inventario.servlets;
+
+import com.sap.conexion.Conexion;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author claudia
+ */
+@WebServlet(name = "AgregarMerma", urlPatterns = {"/AgregarMerma"})
+public class AgregarMerma extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ClassNotFoundException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+       Conexion c = new Conexion();
+        String mclave = request.getParameter("clavem");
+        String mproducto = request.getParameter("producto");
+        String mcantidad = request.getParameter("cantidad");
+    String mdescripcion = request.getParameter("descripcion");
+        String mfecha = request.getParameter("fecha");
+        String mtipo = request.getParameter("mermatipo");
+         c.insertar( 
+                "clave_merma,produto,cantidad,descripcion,fecha,tipo_merma","merma",
+                "'"+mclave+"','" + mproducto+"'," + mcantidad+",'" + mdescripcion+"','" + mfecha+"','" + mtipo + "'"
+        );
+
+        response.sendRedirect("Inventario/InventarioMermaAgregar.jsp");
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(AgregarMerma.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(AgregarMerma.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/java/com/sap/inventario/servlets/ModificarProducto.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sap.inventario.servlets;
+
+import com.sap.conexion.Conexion;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author claudia
+ */
+@WebServlet(name = "ModificarProducto", urlPatterns = {"/ModificarProducto"})
+public class ModificarProducto extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ClassNotFoundException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        Conexion c=new Conexion();
+        String clave = request.getParameter("clave");
+        String nombre = request.getParameter("nombre");
+        String tipo = request.getParameter("tipo");
+        String unidad = request.getParameter("unidad");
+        String cantidad = request.getParameter("cantidad");
+        String costounitario = request.getParameter("costo");
+        String iva = request.getParameter("iva");
+        String fecha= request.getParameter("fecha");
+        String operacion= request.getParameter("operacion");
+        c.insertar("clave,nombre,tipo,unidad,cantidad,costounitario,iva,fecha,operacion", "producto",
+                    "'"+clave+"','"+nombre+"','"+tipo+"','"+unidad+"',"+cantidad+","+costounitario+","+iva+",'"+fecha+"','"+operacion+"'");
+         response.sendRedirect("Inventario/InventarioProductoModificar.jsp");
+    }
+
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ModificarProducto.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ModificarProducto.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
